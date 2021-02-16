7376444d50cd46315e4ac0da4c2c21ae9db34a38
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-02-16 08:52:24 -0600

    Fixed aggregate function

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -6,64 +6,17 @@ module.exports = (app) => {
 
     app.get("/api/workouts", ({body}, res) => {
 
-        Workout.find({}, (err, data) => {
-            console.log(JSON.stringify(data[4]));
-            
-            // res.json(data)
-        })
-
-        // console.log("*********************************************************************");
         Workout.aggregate([
             {
-                "$unwind": "$exercises"
-            },
-            {
-                $group: {
-                    if: {"$eq": ["$exercises.type", "resistance"]},
-                    then: {
-                        weight: {$sum: "$exercises.weight"},
-                        sets: {$sum: "$exercises.sets"},
-                        reps: {$sum: "$exercises.reps"},
-                        duration: {$sum: "$exercises.duration"}
-                    },
-                    else: {
-                        duration: {$sum: "$exercises.duration"},
-                        distance: {$sum: "$exercises.distance"}
+                $addFields: {
+                    totalDuration: {$sum: "$exercises.duration"}
                     }
-                },
                 
-            },
-            {
-                $group: {
-                    exercises: {
-                        $push: {
-                            if: {"$eq": ["$exercises.type", "resistance"]},
-                            then: {
-                                type: "$type",
-                                name: "$name",
-                                weight: "$weight",
-                                sets: "$sets",
-                                reps: "$reps",
-                                duration: "$duration",
-                                distance: "$distance"
-                            },
-                            else: {
-                                type: "$type",
-                                name: "$name",
-                                duration: "$duration",
-                                distance: "$distance"
-                            }
-                        }
-                    },
-                    _id: "$_id",
-                    day: "$day"
-                }
             }
 
         ], (err, data) => {
-            // if(err) throw err;
-            console.log(data + " hereerererrrrrrrrrr");
-            // res.json(data)
+            if(err) throw err;
+            res.json(data)
         })
         
     })
