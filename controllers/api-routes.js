ce1570088635331d2cc8a3a237a6e0543db751c9
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2021-02-16 08:32:01 -0600

    Keep github up to date

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -7,29 +7,64 @@ module.exports = (app) => {
     app.get("/api/workouts", ({body}, res) => {
 
         Workout.find({}, (err, data) => {
-            // console.log(data);
-            res.json(data)
+            console.log(JSON.stringify(data[4]));
+            
+            // res.json(data)
         })
 
         // console.log("*********************************************************************");
-        // Workout.aggregate([
-        //     {
-        //         "$unwind": "$exercises"
-        //     },
-        //     {$group: {
-        //         _id: "$_id",
-        //         weight: {$sum: "$exercises.weight"},
-        //         sets: {$sum: "$exercises.sets"},
-        //         reps: {$sum: "$exercises.reps"},
-        //         duration: {$sum: "$exercises.duration"},
-        //         distance: {$sum: "$exercises.distance"}
-        //         }
-        //     }
+        Workout.aggregate([
+            {
+                "$unwind": "$exercises"
+            },
+            {
+                $group: {
+                    if: {"$eq": ["$exercises.type", "resistance"]},
+                    then: {
+                        weight: {$sum: "$exercises.weight"},
+                        sets: {$sum: "$exercises.sets"},
+                        reps: {$sum: "$exercises.reps"},
+                        duration: {$sum: "$exercises.duration"}
+                    },
+                    else: {
+                        duration: {$sum: "$exercises.duration"},
+                        distance: {$sum: "$exercises.distance"}
+                    }
+                },
+                
+            },
+            {
+                $group: {
+                    exercises: {
+                        $push: {
+                            if: {"$eq": ["$exercises.type", "resistance"]},
+                            then: {
+                                type: "$type",
+                                name: "$name",
+                                weight: "$weight",
+                                sets: "$sets",
+                                reps: "$reps",
+                                duration: "$duration",
+                                distance: "$distance"
+                            },
+                            else: {
+                                type: "$type",
+                                name: "$name",
+                                duration: "$duration",
+                                distance: "$distance"
+                            }
+                        }
+                    },
+                    _id: "$_id",
+                    day: "$day"
+                }
+            }
 
-        // ], (err, data) => {
-        //     console.log(data);
-        //     // res.json(data)
-        // })
+        ], (err, data) => {
+            // if(err) throw err;
+            console.log(data + " hereerererrrrrrrrrr");
+            // res.json(data)
+        })
         
     })
 
